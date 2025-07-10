64726635608e658f96a86e9e15d5bc98b1bc53b2
6261vishnumho <7225vishnumahor@gmail.com>
2025-07-10 13:38:47 +0530

    add files

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -4,7 +4,7 @@ import { CiHeart } from "react-icons/ci";
 import { IoIosSearch } from "react-icons/io";
 import { FaRegUser } from "react-icons/fa";
 import { CiShoppingCart } from "react-icons/ci";
-import {  useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { CiDark } from "react-icons/ci";
 import { CiLight } from "react-icons/ci";
 import { useContext } from "react";
@@ -12,12 +12,13 @@ import { MyContext } from "./context.jsx";
 
 
 const Navbar = () => {
-     const {s, setS, dark}= useContext(MyContext)
-    const v=useNavigate()
-   
+    const { s, setS, dark } = useContext(MyContext)
+    const v = useNavigate()
+
     const [c1, setC1] = useState("none")
     const [c2, setC2] = useState("none")
-    
+    const [c3, setC3] = useState("none")
+
     const ref = useRef();
 
     const r = () => {
@@ -32,25 +33,30 @@ const Navbar = () => {
     const f = () => {
         setC2("none")
     }
-   
-    
+const a=()=>{
+    setC3("flex")
+}
+const a1=()=>{
+    setC3("none")
+}
+
     const h = () => {
         ref.current.style.color = ' rgb(29, 29, 29)'
     }
-   
+
 
     return (
         <>
-       
-            <section className="sec1"style={{ color: (s % 2 == 0) ? "rgb(88, 88, 90)" : "white", background: (s % 2 == 0) ? "white" : "rgb(87, 87, 89)" }} >
+
+            <section className="sec1" style={{ color: (s % 2 == 0) ? "rgb(88, 88, 90)" : "white", background: (s % 2 == 0) ? "white" : "rgb(87, 87, 89)" }} >
                 <div className="div1" onMouseLeave={f}>
                     <Link className="l1" to="/">home</Link>
-                <div onMouseLeave={f}>
-                    <div className="l1" onMouseEnter={r} >Categories  </div>
+                    <div onMouseLeave={f}>
+                        <div className="l1" onMouseEnter={r} >Categories  </div>
                     </div>
 
                     <div className="l1" onMouseEnter={e} >features</div>
-                    <div className="l1" to="/Product" >product</div>
+                    <div className="l1" to="/Product" onMouseEnter={a}>product</div>
                     {/* <Link className="l1" to="/Pages" >pages</Link> */}
                     {/* <Link className="l1" to="/Element" >Element</Link> */}
                 </div>
@@ -61,7 +67,7 @@ const Navbar = () => {
                     <Link className="l2" to="USD"> USD</Link>
                     <Link className="l2" to="ELG"> ELG</Link>
                     <Link className="l2" to="More"> More</Link>
-                    {(s%2==0)?<button onClick={dark}><CiDark /></button>:<button onClick={dark}><CiLight /></button>}
+                    {(s % 2 == 0) ? <button onClick={dark}><CiDark /></button> : <button onClick={dark}><CiLight /></button>}
                     <Link className="l3"> <IoIosSearch /></Link>
                     <Link className="l3"><CiHeart /></Link>
                     <Link className="l3" to="Login"> <FaRegUser /></Link>
@@ -130,38 +136,160 @@ const Navbar = () => {
 
             <div className="categories2" onMouseEnter={e} onMouseLeave={f} style={{ display: c2 }}>
                 <div className="g">
-                <div>
-                    <Link className="var3" to="Header Builder & TypesNEW">HEADER BUILDER & TYPES </Link>
-                </div>
-                <div>
-                    <Link className="var3" to="Custom Product LayoutNEW">CUSTOM PRODUCT LAYOUT</Link>
-                </div>
-                <div>
-                    <Link className="var3" to="Porto Studio">PORTO STUDIO</Link>
-                </div>
-                <div>
-                    <Link className="var3" to="Whats New">WHAT'S NEW</Link>
-                </div>
-                <div>
-                    <Link className="var3" to="Top Performance">TOP PERFORMANCE</Link>
-                </div>
-                <div>
-                    <Link className="var3" to="Speed Optimize Wizard">SPEED OPTIMIZE WIZARD</Link>
-                </div>
-                <div>
-                    <Link className="var3" to="One Click Install Demo">ONE CLICK INSTALL DEMO</Link>
-                </div>
-                <div>
-                    <Link className="var3" to="Single Page Navigation">SINGLE PAGE NAVIGATION</Link>
-                </div>
-                <div>
-                    <Link className="var3" to="Live Search">LIVE SEARCH</Link>
-                </div>
-                <div>
-                    <Link className="var3" to="Skeleton Screens">SKELETON SCREENS</Link>
-                </div>
+                    <div>
+                        <Link className="var3" to="Header Builder & TypesNEW">HEADER BUILDER & TYPES </Link>
+                    </div>
+                    <div>
+                        <Link className="var3" to="Custom Product LayoutNEW">CUSTOM PRODUCT LAYOUT</Link>
+                    </div>
+                    <div>
+                        <Link className="var3" to="Porto Studio">PORTO STUDIO</Link>
+                    </div>
+                    <div>
+                        <Link className="var3" to="Whats New">WHAT'S NEW</Link>
+                    </div>
+                    <div>
+                        <Link className="var3" to="Top Performance">TOP PERFORMANCE</Link>
+                    </div>
+                    <div>
+                        <Link className="var3" to="Speed Optimize Wizard">SPEED OPTIMIZE WIZARD</Link>
+                    </div>
+                    <div>
+                        <Link className="var3" to="One Click Install Demo">ONE CLICK INSTALL DEMO</Link>
+                    </div>
+                    <div>
+                        <Link className="var3" to="Single Page Navigation">SINGLE PAGE NAVIGATION</Link>
+                    </div>
+                    <div>
+                        <Link className="var3" to="Live Search">LIVE SEARCH</Link>
+                    </div>
+                    <div>
+                        <Link className="var3" to="Skeleton Screens">SKELETON SCREENS</Link>
+                    </div>
                 </div>
             </div>
+            <section className="product" onMouseEnter={a} onMouseLeave={a1} style={{display:c3}}>
+                <article className="product1">
+                    <div>
+                        <Link className="var4" to="Product Pages">Product Pages</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Simple Product">Simple Product</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Variable Product">Variable Product</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Combo Products">Combo Products</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Countdown Sale">Countdown Sale</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="With Custom Tab">With Custom Tab</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Frequently Together">Frequently Together</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Advanced Review">Advanced Review</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Add Cart Sticky">Add Cart Sticky</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Image Swatch">Image Swatch</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Scattered LayoutNew">Scattered LayoutNew</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Video ThumbnailNew">Video ThumbnailNew</Link>
+                    </div>
+                </article>
+                <article className="product1">
+                    <div>
+                        <Link className="var4" to="Product Layouts">Product Layouts</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Extended Layout">Extended Layout</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Grid Images">Grid Images</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Sticky Info">Sticky Info</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Left & Right Sticky">Left & Right Sticky</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Transparent Images">Transparent Images</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Center Vertical Thumb">Center Vertical Thumb</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Center Full Gallery">Center Full Gallery</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Half Full Gallery">Half Full Gallery</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Boxed Content">Boxed Content</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="360 DegreeNew">360 DegreeNew</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Shipping DeliveryNew">Shipping DeliveryNew</Link>
+                    </div>
+                </article>
+                <article className="product1">
+                    <div>
+                        <Link className="var4" to="Product Loop">Product Loop</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Full Block">Full Block</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Quantity Input">Quantity Input</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Image Bottom">Image Bottom</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Details On Image">Details On Image</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Add To Cart Link">Add To Cart Link</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Box Hover EffectsNew">Box Hover EffectsNew</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Box Shadow Details">Box Shadow Details</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Box Border">Box Border</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Hidden Details">Hidden Details</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Overlay DetailsNew">Overlay DetailsNew</Link>
+                    </div>
+                    <div>
+                        <Link className="var4" to="Button And IconsNew">Button And IconsNew</Link>
+                    </div>
+                </article>
+                <article className="product2">
+                    <img src="https://www.portotheme.com/wordpress/porto/shop3/wp-content/uploads/sites/214/2023/10/shop3-menu-banner-2.jpg?id=2247" alt="" />
+                    <div id="up">UP TO</div>
+                    <div id="up1">50%</div>
+                    <button>VIEW SALE</button>
+                </article>
+            </section >
         </>
     )
 }
--- src/nextfiles/shopNow.jsx
+++ src/nextfiles/shopNow.jsx
@@ -3,37 +3,57 @@ import { GoPlus } from "react-icons/go";
 import { VscDash } from "react-icons/vsc";
 import "./shopnow.css"
 import { useState } from "react";
-const Shopnow=()=>{
-    const [v,setV]=useState(0)
-    const plus=()=>{
-setV(s+1)
+const Shopnow = () => {
+    const [v, setV] = useState(0)
+    const plus = () => {
+        setV(v+1)
     }
-    return(
+    const plus1 = () => {
+        setV("block")
+    }
+    return (
         <>
-        <div>HOME<FiChevronLeft />SHOP </div>
-        <div id="v"></div>
-        <section className="v4">
-            <div className="v5">
-           <div ><a href="#">Categories</a></div>
-           <div><GoPlus/></div>
-           </div>
-            <ul  onClick={plus}>
-            <li className="v3"></li>
-            <li className="v3"><a href="">Accesaries</a></li>
-            <li className="v3"><a href="">Bags</a></li>
-            <li className="v3"><a href="">Hates</a></li>
-            <li className="v3"><a href="">Jakets</a></li>
-            <li className="v3"><a href="">Shose</a></li>
-            <li className="v3"><a href="">Sunglashes</a></li>
-            </ul>
-          
-        </section>
-        <div id="v2"></div>
-        <div>Price</div>
-        <div>
-           <input type="range" name="range" />
+            <div>HOME<FiChevronLeft />SHOP </div>
+            <div id="v"></div>
+            <section className="v4">
+
+                <div className="v5">
+
+                    <div ><a href="#">Categories</a></div>
+                    <div>
+                        <div>
+                            {(v%2==0)? <div onClick={plus}><VscDash /></div>:<div onClick={plus}><GoPlus /></div>}
+                        </div>
+                        {/* <div onClick={plus}><VscDash /></div>
+                        <div onClick={plus1} style={{ display: setV }}> <GoPlus /></div> */}
+                    </div>
+
+                </div>
+
+                <div >
+                    <ul {(v%2==0)?style}>
+                        <li className="v3"><a href="">Accesaries</a></li>
+                        <li className="v3"><a href="">Bags</a></li>
+                        <li className="v3"><a href="">Hates</a></li>
+                        <li className="v3"><a href="">Jakets</a></li>
+                        <li className="v3"><a href="">Shose</a></li>
+                        <li className="v3"><a href="">Sunglashes</a></li>
+
+                    </ul>
+                </div>
+            
+            <div id="v2"></div>
+            <div>Price</div>
+            <div className="v6">
+                <input type="range" name="range" />
             </div>
-        
+            <div className="v7">
+                <div>
+                    Price: $30 — $300
+                </div>
+                <button>FILTER</button>
+            </div>
+            </section >
         </>
     )
 }
